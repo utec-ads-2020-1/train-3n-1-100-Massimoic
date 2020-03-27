a791d199a40186c6963ee66beef25b0a7868018d
Massimo Imparato <massimo.imparato@utec.edu.pe>
2020-03-26 20:10:05 -0500

    Terminado

--- main.cpp
+++ main.cpp
@@ -2,8 +2,34 @@
 #include <vector>
 using namespace std;
 
+int cycle(int num) {
+    int contador = 1;
+
+    if(num != 1 && num > 0 ) {
+        while( num != 1) {
+            if( num % 2 != 0 ) {
+                num = 3*num +1;
+            }
+            else
+                num = num/2;
+            contador++;
+        }
+    }
+    if( num == 1)
+        return contador;
+    else
+        return 0;
+}
+
 int findLength(int i ,int j) {
-    
+    int currentMax = 0;
+    cout << i << ' ' << j;
+
+    for (i ; i <= j ; i++) {
+        if(cycle(i) > currentMax)
+            currentMax = cycle(i);
+    }
+    return currentMax;
 }
 
 int main(){
